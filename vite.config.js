41013eb8742487689a5d7ab27d7b30bfc7d43dbe
DawnMagnet <axccjqh@qq.com>
2025-06-03 15:11:52 +0800

    replace with rolldown-vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'rolldown-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   root: '.',
-  base: '/weaveup/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
